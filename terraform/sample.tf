3f00066aad6008f3e0a633cc9a80e3815926b99c
PavanS2002 <109422438+PavanS2002@users.noreply.github.com>
2024-02-13 19:10:29 +0530

    3

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -1,14 +1,9 @@
 provider "aws" {
     region = "ap-south-1"
 }
-
-resource  "aws_instance" "my_instance" {
-    ami = "ami-03f4878755434977f"
-    instance_type = "t2.micro"
-    key_name = "1.mum"
-    vpc_security_group_ids = [sg-028e2dc3ff1d822ed]
-    tags = {
-        Nmae = "Pavan"
-        env = "dev"
-    }
+resource "aws_instance" "my_instance" {
+     ami = "ami-03f4878755434977f"
+     instance_type = "t2.micro"
+     key_name = "1.mum"
+     vpc_security_group_ids = ["sg-028e2dc3ff1d822ed"]
 }
