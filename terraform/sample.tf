df9baa3621b8da38295d7ac8aee5ed0f50f9aab2
PavanS2002 <109422438+PavanS2002@users.noreply.github.com>
2024-02-13 19:35:42 +0530

    new file acdded

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -1,9 +1,54 @@
 provider "aws" {
     region = "ap-south-1"
 }
+
+#Create Security Group
+
+resource "aws_security_group" "my_sg" {
+    name = "Terraform"
+    description = "allow-ssh adn http"
+    vpc_id = var.vpc_security_group_ids
+    ingress {
+        protocol = "TCP"
+        from_port = 22
+        to_port = 22
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        protocol = "TCP"
+        from_port = 80
+        to_port = 80
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+#Launch AWS instance
+
 resource "aws_instance" "my_instance" {
-     ami = "ami-03f4878755434977f"
-     instance_type = "t2.micro"
+     ami = var.image_id
+     instance_type = varr.instance_type
      key_name = "1.mum"
      vpc_security_group_ids = ["sg-028e2dc3ff1d822ed"]
+}
+
+resource "aws_instance" "my_instance" {
+     ami = var.image_id
+     instance_type = varr.instance_type
+     key_name = "1.mum"
+     vpc_security_group_ids = ["sg-028e2dc3ff1d822ed", ]
+}
+
+
+# Variables
+
+Variables "image_id" {
+    default = "03f4878755434977f"
+}
+
+Variables "instance_type" {
+    default = "t2.micro"
+}
+
+Variables "vpc_security_group_ids" {
+    default = "sg-028e2dc3ff1d822ed"
 }
