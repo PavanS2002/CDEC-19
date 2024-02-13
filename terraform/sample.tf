24013165fcfa23bb0310e92c777aca9f0cce8b0f
PavanS2002 <109422438+PavanS2002@users.noreply.github.com>
2024-02-13 18:35:17 +0530

    2

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-resource = "aws_instance" "my_instance" {
+resource  "aws_instance" "my_instance" {
     ami = "ami-03f4878755434977f"
     instance_type = "t2.micro"
     key_name = "1.mum"
