8ed4d80741c0098b3ecefefc96a709200e4e5e59
PavanS2002 <109422438+PavanS2002@users.noreply.github.com>
2024-02-14 10:14:33 +0530

    2

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_security_group" "my_sg" {
     name = "Terraform"
     description = "allow-ssh adn http"
-    vpc_id = var.vpc_security_group_ids
+    vpc_id = var.vpc_id
     ingress {
         protocol = "TCP"
         from_port = 22
@@ -26,16 +26,16 @@ resource "aws_security_group" "my_sg" {
 
 resource "aws_instance" "my_instance" {
      ami = var.image_id
-     instance_type = varr.instance_type
+     instance_type = var.instance_type
      key_name = "1.mum"
      vpc_security_group_ids = ["sg-028e2dc3ff1d822ed"]
 }
 
-resource "aws_instance" "my_instance" {
+resource "aws_instance" "new_instance" {
      ami = var.image_id
-     instance_type = varr.instance_type
+     instance_type = var.instance_type
      key_name = "1.mum"
-     vpc_security_group_ids = ["sg-028e2dc3ff1d822ed", Terraform ]
+     vpc_security_group_ids = ["sg-028e2dc3ff1d822ed", "Terraform" ]
 }
 
 
@@ -49,6 +49,6 @@ Variables "instance_type" {
     default = "t2.micro"
 }
 
-Variables "vpc_security_group_ids" {
+Variables "vpc_id" {
     default = "sg-028e2dc3ff1d822ed"
 }
