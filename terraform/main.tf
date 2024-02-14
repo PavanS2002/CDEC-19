402bec5a0bf228a30c34269fdb467216a0578297
PavanS2002 <109422438+PavanS2002@users.noreply.github.com>
2024-02-14 19:08:50 +0530

    change

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,7 @@ resource "aws-subnet" "public_subnet" {
     }
 }
 
-resource = "aws_internet_gateway" "my_igw" {
+resource "aws_internet_gateway" "my_igw" {
     vpc_id = aws_vpc.my_vpc.id
     tags = {
         Name = "${var.project}-igw"
