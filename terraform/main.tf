782382e36e3d61b6bd87e25b20949f87a8ac9b74
PavanS2002 <109422438+PavanS2002@users.noreply.github.com>
2024-02-14 19:11:25 +0530

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ resource "aws_subnet" "private_subnet" {
     } 
 }
 
-resource "aws-subnet" "public_subnet" {
+resource "aws_subnet" "public_subnet" {
     vpc_id     = aws_vpc.my_vpc.id
     cidr_block = var.public_subnet_cidr
     map_public_ip_on_launch = true
