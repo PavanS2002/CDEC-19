94a23e03f89f17af24ef5501be7d963600df2791
PavanS2002 <109422438+PavanS2002@users.noreply.github.com>
2024-02-16 16:10:24 +0530

    2

--- terraform/homework 1/user.tf
+++ terraform/homework 1/user.tf
@@ -41,5 +41,9 @@ resource "aws_s3_bucket_policy" "my_bucket_policy" {
 #policy attach to user
 resource "aws_iam_user_policy_attachment" "attachment-policy" {
   user       = aws_iam_user.my_user.name
+<<<<<<< HEAD
+  policy_arn = aws_iam_policy.new-bucket.arn
+=======
   policy_arn = aws_iam_policy.terraform-bucket.arn
+>>>>>>> 9f74229c1e89fd615d0e917c6476f984bb2a679a
 }
