2b3c39fd3522cdb4ad314042bfd21e74750a2feb
PavanS2002 <109422438+PavanS2002@users.noreply.github.com>
2024-02-16 15:37:02 +0530

    1

--- terraform/homework 1/user.tf
+++ terraform/homework 1/user.tf
@@ -2,7 +2,35 @@ provider "aws" {
     region = "ap-south-1"
 }
 
+# Craete User
 resource "aws_iam_user" "my_user" {
     name = "my-user"
     path = "/system/"
+}
+
+#Create S3 Bucket 
+resoure "aws_s3_bucket" "new_bucket" {
+    bucket = "new-bucket"
+    acl = "private"
+    tags = {
+        Name = "new-bucket"
+        env = "dev"
+    }
+} 
+
+#Create Policy For Bucket
+resource "aws_s3_bucket_policy" "amazon" {
+    bucket = aws_s3_bucket.amazon
+    policy = <<EOF {
+        version = "2012-10-17"
+        statement = [{
+            Effect = "Allow"
+            Principal = "*"
+            Action = [
+                "s3:GetObject" , "s3:ListBucket" , "s3:PutObject"
+            ]
+            Resource = "arn:aws:s3::new-bucket"
+        }
+        ]
+    }
 }
