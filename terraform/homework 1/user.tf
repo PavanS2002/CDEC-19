c6ae4abe62359f41190d28da27b21b03509c0bb4
PavanS2002 <109422438+PavanS2002@users.noreply.github.com>
2024-02-16 14:14:46 +0530

    1

---
+++ terraform/homework 1/user.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "ap-south-1"
+    access_key_id = "AKIA2UC27APNWLF2YK3F"
+    secret_acces_key_id = "ew9ZaObhFxa+/+BawjM24PErErWtaOI62uiQLgBM"
+}
+
+resource "aws_iam_user" "my_user" {
+    name = "my-user"
+    path = /system/
+}
