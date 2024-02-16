e2ef3471eaa243f7fc1697c30453a7f085532a05
PavanS2002 <109422438+PavanS2002@users.noreply.github.com>
2024-02-16 14:47:00 +0530

    3

--- terraform/homework 1/user.tf
+++ terraform/homework 1/user.tf
@@ -1,7 +1,5 @@
 provider "aws" {
     region = "ap-south-1"
-    access_key = "AKIA2UC27APNWLF2YK3F"
-    secret_key = "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"
 }
 
 resource "aws_iam_user" "my_user" {
