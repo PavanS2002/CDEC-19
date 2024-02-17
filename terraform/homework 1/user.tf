f5ebd742e3229bbd9de7ac0c78bd7537b11fa631
PavanS2002 <109422438+PavanS2002@users.noreply.github.com>
2024-02-17 16:06:51 +0530

    1

--- terraform/homework 1/user.tf
+++ terraform/homework 1/user.tf
@@ -17,33 +17,3 @@ resource "aws_s3_bucket" "new_bucket" {
         env = "dev"
     }
 } 
-
-#Create Policy For Bucket
-resource "aws_s3_bucket_policy" "my_bucket_policy" {
-  bucket = "terraform-bucket"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = "*",
-        Action = [
-          "s3:GetObject"
-          ,"S3:PutObject"
-          ,"S3:ListObject"
-        ],
-        Resource = "arn:aws:s3:::new-bucket/*"
-      }
-    ]
-  })
-}
-
-#policy attach to user
-resource "aws_iam_user_policy_attachment" "attachment-policy" {
-  user       = aws_iam_user.my_user.name
-  policy_arn = aws_s3_bucket_policy.my_bucket_policy.arn
-}
-
-output "iam_user_name" {
-  value = aws_iam_user.my_user.name
-}
