2b8f597df764d67fee2ee12eefba096c12ae56e9
PavanS2002 <109422438+PavanS2002@users.noreply.github.com>
2024-02-17 16:16:41 +0530

    2

--- terraform/homework 1/user.tf
+++ terraform/homework 1/user.tf
@@ -9,11 +9,20 @@ resource "aws_iam_user" "my_user" {
 }
 
 #Create S3 Bucket 
-resource "aws_s3_bucket" "new_bucket" {
-    bucket = "new-bucket"
-    aws_s3_bucket_acl = "private"
-    tags = {
-        Name = "new-bucket"
-        env = "dev"
-    }
-} 
+resource "aws_s3_bucket" "  Pavan_bucket" {
+  bucket = "Pavan_bucket"
+}
+
+resource "aws_s3_bucket_ownership_controls" "Pavan_bucket" {
+  bucket = aws_s3_bucket.Pavan_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "Pavan" {
+  depends_on = [aws_s3_bucket_ownership_controls.Pavan_bucket]
+
+  bucket = aws_s3_bucket.Pavan_bucket.id
+  acl    = "private"
+}
