4222360edb99216e05efda127eff3f219a1d9d29
PavanS2002 <109422438+PavanS2002@users.noreply.github.com>
2024-02-22 18:36:29 +0530

    5

--- terraform/eks/eks-cluster.tf
+++ terraform/eks/eks-cluster.tf
@@ -42,17 +42,17 @@ data "aws_iam_policy_document" "assume_role" {
 
 resource "aws_iam_role" "eks_cluster_role" {
     name = "eks-cluster-role"
-    assume_role_policy = data.aws_iam_policy_document.assum_role.json
+    assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
 resource "aws_iam_role_policy_attachment" "eks_k8s" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = data.aws_iam_policy_document.assume_role.name
+  role       = aws_iam_role.eks_cluster_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cluster" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = data.aws_iam_policy_document.assume_role.name
+  role       = aws_iam_role.eks_cluster_role.name
 }
 #cluster
 resource "aws_eks_cluster" "first_cluster" {
