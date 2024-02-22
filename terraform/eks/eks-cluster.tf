fd47fc694a9a92cd4059f4db2054c72fca4f8838
PavanS2002 <109422438+PavanS2002@users.noreply.github.com>
2024-02-22 19:07:21 +0530

    9

--- terraform/eks/eks-cluster.tf
+++ terraform/eks/eks-cluster.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "eks_vpc" {
 
 resource "aws_subnet" "subnet_1" {
     vpc_id = aws_vpc.eks_vpc.id
-    cidr_block = "172.31.16.0/22"
+    cidr_block = "169.254.0.0/20"
     tags = {
         Name = "subnet-1"
         env = "dev"
@@ -22,7 +22,7 @@ resource "aws_subnet" "subnet_1" {
 
 resource "aws_subnet" "subnet_2" {
     vpc_id = aws_vpc.eks_vpc.id
-    cidr_block = "172.31.32.0/20"
+    cidr_block = "169.254.0.16/20"
     tags = {
         Name = "subnet-2"
         env = "dev"
