58876f9bb0fc91ddc2523c3702c88ca87c0b97b8
PavanS2002 <109422438+PavanS2002@users.noreply.github.com>
2024-02-22 18:46:03 +0530

    1

--- terraform/eks/eks-cluster.tf
+++ terraform/eks/eks-cluster.tf
@@ -58,8 +58,8 @@ resource "aws_iam_role_policy_attachment" "eks_cluster" {
 resource "aws_eks_cluster" "first_cluster" {
     name = "new_cluster"
     role_arn = aws_iam_role.eks_cluster_role.arn
-}
 
 vpc_config {
     subnet_ids = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
+  }
 }
